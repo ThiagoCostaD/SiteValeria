3f7b76b5eadc4aac8231457997c9befa3f2779ab
Thiago Costa <thiagoocdiniz@gmail.com>
2023-06-30 10:41:53 -0300

    criando parciais

--- testemunhos/views.py
+++ testemunhos/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'testemunhos/home.html', context={
+    return render(request, 'testemunhos/pages/home.html', context={
 
     })
