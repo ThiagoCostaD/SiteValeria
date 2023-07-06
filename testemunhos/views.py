439cfe20dc0ac6bbb31927b122c148176d48d538
Thiago Costa <thiagoocdiniz@gmail.com>
2023-07-06 01:08:12 -0300

    fabricando conteudo para teste

--- testemunhos/views.py
+++ testemunhos/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
+from utils.testemunhos.fabrica import make_testemunho
 
 
 def home(request):
     return render(request, 'testemunhos/pages/home.html', context={
-
-    })
+        'testemunhos': [make_testemunho() for _ in range(7)], })
 
 
 def testemunho(request, id):
     return render(request, 'testemunhos/pages/testemunho-view.html', context={
-
+        'testemunho': make_testemunho(),
     })
