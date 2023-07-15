128dd466da561a4ea5c1ff8025b98359d384f4b6
Thiago Costa <thiagoocdiniz@gmail.com>
2023-07-15 02:10:37 -0300

    fazendo testes

--- testemunhos/models.py
+++ testemunhos/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(
+        max_length=50, help_text='Nome da categoria')
 
     def __str__(self):
         return self.nome
@@ -11,22 +12,37 @@ class Categoria(models.Model):
 
 class Testemunho(models.Model):
 
-    titulo = models.CharField(max_length=77)
-    descricao = models.CharField(max_length=165)
-    slug = models.SlugField()
-    testemunho = models.TextField()
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_modificacao = models.DateTimeField(auto_now=True)
-    publicado = models.BooleanField(default=False)
-    foto = models.ImageField(upload_to='testemunhos/img/%Y/%m/%d/')
+    titulo = models.CharField(
+        max_length=77, help_text='Título do testemunho', blank=False)
+    descricao = models.CharField(
+        max_length=165, help_text='Descrição do testemunho', blank=False)
+    slug = models.SlugField(
+        help_text='Slug do testemunho', blank=False, unique=True)
+    testemunho = models.TextField(help_text='Texto do testemunho', blank=False)
+    data_criacao = models.DateTimeField(
+        auto_now_add=True, help_text='Data de criação do testemunho')
+    data_modificacao = models.DateTimeField(
+        auto_now=True, help_text='Data de modificação do testemunho')
+    publicado = models.BooleanField(
+        default=False,
+        help_text='Indica se o testemunho está publicado ou não')
+    foto = models.ImageField(
+        upload_to='testemunhos/img/%Y/%m/%d/', help_text='Foto do testemunho')
     categoria = models.ForeignKey(
         Categoria, on_delete=models.SET_NULL, null=True,
-        blank=True, default=None,
+        blank=True, default=None, help_text='Categoria do testemunho',
+        related_name='testemunhos', related_query_name='testemunho_categoria'
     )
     autor = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True,
-        blank=True, default=None
+        blank=True, default=None, help_text='Autor do testemunho',
+        related_name='testemunhos', related_query_name='testemunho_autor'
     )
+
+    class Meta:
+        verbose_name = 'Testemunho'
+        verbose_name_plural = 'Testemunhos'
+        ordering = ['-data_criacao']
 
     def __str__(self):
         return self.titulo
--- testemunhos/tests.py
+++ testemunhos/tests.py
@@ -1,6 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 
 class TestemunhoURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1, 'Um é igual a um'
+
+    def test_testemunho_home_url_esta_correto(self):
+        url = reverse('testemunhos:home')
+        self.assertEqual(url, '/')
+
+    def test_testemunho_categoria_url_esta_correto(self):
+        url = reverse('testemunhos:categoria', kwargs={'categoria_id': 1})
+        self.assertEqual(url, '/testemunhos/categoria/1/')
+
+    def test_testemunho_url_esta_correto(self):
+        url = reverse('testemunhos:testemunho', args=(1,))
+        self.assertEqual(url, '/testemunhos/1/')
+
+
+class TestemunhoViewsTest(TestCase):
+
+    def test_testemunho_home_view_esta_funcionando(self):
+        view = resolve('/')
+        self.assertIs()
