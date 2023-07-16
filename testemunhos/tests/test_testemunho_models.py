04a5d8bc50cb678aff943fe545f6858cf2e6a980
Thiago Costa <thiagoocdiniz@gmail.com>
2023-07-16 17:51:26 -0300

    ValidationError

--- testemunhos/tests/test_testemunho_models.py
+++ testemunhos/tests/test_testemunho_models.py
@@ -1,3 +1,5 @@
+from django.core.exceptions import ValidationError
+
 from .test_testemunho_base import TestemunhoTestBase
 
 
@@ -5,3 +7,9 @@ class TestemunhoModelTest(TestemunhoTestBase):
     def setUp(self) -> None:
         self.testemunho = self.make_testemunho()
         return super().setUp()
+
+    def test_titulo_testemunho_erro_acima_de_77_caracteres(self):
+        self.testemunho.titulo = 'A' * 78
+
+        with self.assertRaises(ValidationError):
+            self.testemunho.full_clean()
