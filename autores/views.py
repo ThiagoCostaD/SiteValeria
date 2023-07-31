e8d32c03857a7da3e72374d421ca485c95332d72
Thiago Costa <thiagoocdiniz@gmail.com>
2023-07-30 22:24:53 -0300

    adicionando campos dos formularios

--- autores/views.py
+++ autores/views.py
@@ -4,7 +4,12 @@ from .forms import RegistroForm
 
 
 def resgistro_views(request):
-    form = RegistroForm()
+
+    if request.POST:
+        form = RegistroForm(request.POST)
+    else:
+        form = RegistroForm()
+
     return render(
         request, 'autores/pages/resgistro_views.html', {
             'form': form,
