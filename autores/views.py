87ff0f566acc422ade53e95f5c452f8ef062e3d2
Thiago Costa <thiagoocdiniz@gmail.com>
2023-07-30 21:38:35 -0300

    iniciando forms

--- autores/views.py
+++ autores/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from .forms import RegistroForm
+
 
 def resgistro_views(request):
-    return render(request, 'autores/pages/resgistro_views.html')
+    form = RegistroForm()
+    return render(
+        request, 'autores/pages/resgistro_views.html', {
+            'form': form,
+        }
+    )
