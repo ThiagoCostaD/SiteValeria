9ce342105216178d6437423a0ea2b0f4e4fd3a2f
Thiago Costa <thiagoocdiniz@gmail.com>
2024-02-18 16:01:53 -0300

    colocando opões na parte de admin do usuario

--- testemunhos/admin.py
+++ testemunhos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Categoria, Testemunho
 
 
@@ -6,9 +7,27 @@ class CategoriaAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Categoria, CategoriaAdmin)
-
-
 @admin.register(Testemunho)
 class TestemunhoAdmin(admin.ModelAdmin):
-    ...
+    list_display = [
+        'id', 'titulo', 'data_criacao', 'data_modificacao', 'publicado', 'categoria', 'autor', 'slug'
+    ]
+
+    list_display_links = 'id', 'titulo',
+
+    search_fields = 'titulo', 'data_criacao', 'categoria', 'autor', 'publicado',
+
+    list_filter = 'data_criacao', 'publicado', 'categoria', 'autor',
+
+    list_per_page = 10
+
+    list_editable = 'categoria', 'publicado',
+
+    ordering = ['-id']
+
+    prepopulated_fields = {
+        "slug": ('titulo',)
+    }
+
+
+admin.site.register(Categoria, CategoriaAdmin)
